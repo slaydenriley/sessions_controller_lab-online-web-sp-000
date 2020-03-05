5e752a8b21c5856b3b11f07a94006af4901a1f79
Riley Slayden <slaydenriley@gmail.com>
2020-03-05 15:56:03 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     return redirect_to(controller: 'sessions',
-                       action: 'new') if !params[:name] || params[:name].empty?
+      action: 'new') if !params[:name] || params[:name].empty?
     session[:name] = params[:name]
     redirect_to controller: 'application', action: 'hello'
   end
